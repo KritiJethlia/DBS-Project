7bf306df09b23ba309557de856f5d3e8776bfd05
Kriti <kriti.jethlia@yahoo.com>
2020-05-17 10:08:59 +0530

    few changes

--- closure.java
+++ closure.java
@@ -6,9 +6,10 @@ public class closure
     //array containing Func Dep as a string  "A,B->C" or "A->B,C"
     static int n;
     static int m;
-    public static void main(String[] args) {
-        input(4,3);  //calling without object
-      }
+    public static void main(String[] args) 
+    {
+        input(6,3);  //calling without object
+    }
 
     public static void calc_closure(String attribute[], String FD[])
     {
@@ -36,12 +37,17 @@ public class closure
                 if(l.indexOf(attribute[i])!=-1)
                 cl[i]++;
             }
+            System.out.println(cl[i]);
         }
         int p=0;
+        int trial = 0;
+        int checker = 0;
         while(p<n)
         {
+            //System.out.print("y");
             while(cl[p]>0)
             {
+                //System.out.print("y");
                 for(i=0;i<n;i++)
                 {   
                     for(j=0;j<m;j++)
@@ -92,8 +98,8 @@ public class closure
                         }
                         else
                         {
-                           
-                            cl[p]--;
+                            trial++;
+                            //cl[p]--;
                             String y1 = FD[j].substring(x+2);
                             String str1[] = y1.split(",",n);
                             for(int k=0;k<str1.length;k++)
@@ -107,22 +113,36 @@ public class closure
                                 if(closure[i].indexOf(str1[k])==-1)//add if it is not present already in closure
                                 {
                                     if(Arrays.asList(attribute).lastIndexOf(str1[k])!=-1)
-                                     {
+                                    {
                                          int index = Arrays.asList(attribute).lastIndexOf(str1[k]);
                                          binaryclo[i]+=(int)Math.pow(2,index);
-                                     }
+                                    }
                                     closure[i] = closure[i]+" "+str1[k];
                                 }
                             }
                         }  
                     }
+                    if(trial==0) //ADDED
+                    {
+                        checker = 1;
+                        continue;
+                    }
+                    if(trial!=0)  //ADDED
+                    {
+                        cl[p]--;
+                        trial = 0;
+                    }
                 }
+                if(checker==1) //ADDED
+                break;
             }
             p++;
+            checker = 0;
         }
         display(closure,attribute,FD);
-        candidateKey(binaryclo, attribute,multipleFD);
+        candidateKey(binaryclo, attribute,FD,multipleFD);
     }
+    
     public static void display(String ptr[], String attribute[], String FD[])
     {
         for(int i=0;i<ptr.length;i++)
@@ -145,11 +165,13 @@ public class closure
         for(int j=0;j<m;j++)
         FD[j]=sc.nextLine();
         calc_closure(attribute,FD);
+        //keybin(attribute,FD);
     }
 
-    public static void candidateKey(int ptr[], String attribute[],ArrayList <ArrayList<Integer>> multipleFD){
+    public static void candidateKey(int ptr[], String attribute[],String FD[],ArrayList <ArrayList<Integer>> multipleFD){
         Vector <Vector<String>> ckey= new Vector<Vector<String>>();
         Vector <Vector<String>> superkey= new Vector<Vector<String>>();
+        //System.out.print("x");
         for(int i=0;i<n;++i)//append individual elements to multipleFD array
         {
             ArrayList<Integer> temp= new ArrayList<Integer>();
@@ -163,7 +185,7 @@ public class closure
             String data[] = new String[i]; 
             combinationUtil(attribute,data,0,n-1,0,i,ckey);//generates all combinations and stores in ckey
         }
-       // for(int i=0;i<multipleFD.size();++i)
+        // for(int i=0;i<multipleFD.size();++i)
         // System.out.println(multipleFD.get(i)+" ");
         for(int i=0;i<ckey.size();i++)//check if they can help in getting all attributes
         {
@@ -194,19 +216,18 @@ public class closure
             }
         }
         System.out.println("Superkeys:");
-    for(int i=0;i<ckey.size();++i)
-    {
-        superkey.add(ckey.elementAt(i));
-        System.out.println(superkey.elementAt(i)+" ");
-    } 
-        
-    for(int i=0;i<n;i++)//
+        for(int i=0;i<ckey.size();++i)
+        {
+            superkey.add(ckey.elementAt(i));
+            System.out.println(superkey.elementAt(i)+" ");
+        } 
+        for(int i=0;i<n;i++)//
         {
             int min=n;
             
             for(int j=0;j<ckey.size();++j)//for each attribute find the min size
             {
-               // System.out.print(ckey.elementAt(j)+" ");
+                // System.out.print(ckey.elementAt(j)+" ");
                 if(ckey.get(j).lastIndexOf(attribute[i])!=-1)
                 {
                     if(ckey.get(j).size() < min)
@@ -225,11 +246,38 @@ public class closure
                 }
             }
         }
-        System.out.println("Cadidate Keys : ");
+        System.out.println("Candidate Keys : ");
         for(int i=0;i<ckey.size();++i)
-        System.out.print(ckey.elementAt(i)+" "); 
-
+        System.out.print(ckey.elementAt(i)+" ");
+        int[] binsuper = new int [superkey.size()];
+        for(int i=0;i<superkey.size();++i)//binary superkeys calculated
+        {
+            int key = 0;
+            for(int j=0;j<superkey.get(i).size();++j)
+            {
+                int index = Arrays.asList(attribute).lastIndexOf(superkey.get(i).get(j));
+                key = key | (int)Math.pow(2,index);
+            }
+            binsuper[i]=key;
+        } 
+        int bincandidate[]  = new int [ckey.size()];//binary candidate keys calculated
+        for(int i=0;i<ckey.size();++i)
+        {
+            int key = 0;
+            for(int j=0;j<ckey.get(i).size();++j)
+            {
+                int index = Arrays.asList(attribute).lastIndexOf(ckey.get(i).get(j));
+                key = key | (int)Math.pow(2,index);
+            }
+            bincandidate[i]=key;
+        }
+        int Bin[][] = new int[m][2];
+        keybin(attribute,FD,Bin);
+        twoNF.check_2NF(Bin,bincandidate,attribute);
+        System.out.println("\n"+"3 NF check");
+        threeNF.check_3NF(Bin,bincandidate,binsuper,attribute);
     }
+    
     static void combinationUtil(String arr[], String data[], int start, int end, int index, int r,Vector <Vector<String>> ckey) 
     { 
         // Current combination is ready to be printed, print it 
@@ -252,4 +300,34 @@ public class closure
             combinationUtil(arr, data, i+1, end, index+1, r,ckey); 
         } 
     } 
+    
+    public static void keybin(String attribute[], String FD[],int Bin[][])
+    {
+        
+        int d = 0;
+        for(int i=0;i<m;i++)
+        {
+            int x = FD[i].indexOf("-");
+            String y = FD[i].substring(0,x);
+            for(int j=0;j<n;j++)
+            {
+                if(y.indexOf(attribute[j])!=-1)
+                d = d|((int)Math.pow(2,j));
+            }
+            Bin[i][0] = (int) d;
+            d=0;
+            y = FD[i].substring(x+2);
+            for(int j=0;j<n;j++)
+            {
+                if(y.indexOf(attribute[j])!=-1)
+                d = d|((int)Math.pow(2,j));
+            }
+            Bin[i][1] = (int) d;
+            d=0;
+        }
+        for(int i=0;i<m;i++)
+        {
+            System.out.println(Bin[i][0]+"->"+Bin[i][1]);
+        }
+    }
 }
--- twoNF.java
+++ twoNF.java
@@ -4,30 +4,31 @@ import java.lang.*;
 public class twoNF
 {
     // public static void main(String[] args) {
-    //     int [][]binaryFD= new int[2][2];
-    //     binaryFD[0][0]=3;
-    //     binaryFD[0][1]=4;
+    //     int [][]binaryFD= new int[5][2];
+    //     binaryFD[0][0]=7;
+    //     binaryFD[0][1]=8;
 
-    //     binaryFD[1][0]=8;
+    //     binaryFD[1][0]=11;
     //     binaryFD[1][1]=16;
 
-    //     // binaryFD[2][0]=4;
-    //     // binaryFD[2][1]=8;
+    //     binaryFD[2][0]=12;
+    //     binaryFD[2][1]=32;
 
-    //     // binaryFD[3][0]=1;
-    //     // binaryFD[3][1]=256;
+    //     binaryFD[3][0]=34;
+    //     binaryFD[3][1]=8;
 
-    //     // binaryFD[4][0]=128;
-    //     // binaryFD[4][1]=512;
-    //     int []candidate= new int[1];
-    //     candidate[0]=11;
-    //     String attribute[] = new String[5];
+    //     binaryFD[4][0]=44;
+    //     binaryFD[4][1]=2;
+    //     int []candidate= new int[2];
+    //     candidate[0]=7;
+    //     candidate[1]=13;
+    //     String attribute[] = new String[6];
     //     attribute[0]="a";
     //     attribute[1]="b";
     //     attribute[2]="c";
     //     attribute[3]="d";
     //     attribute[4]="e";
-    //     // attribute[5]="f";
+    //      attribute[5]="f";
     //     // attribute[6]="g";
     //     // attribute[7]="h";
     //     // attribute[8]="i";
@@ -46,47 +47,50 @@ public class twoNF
         }
         for(int i=0;i<binaryFD.length;++i)
         {
-           if((binaryFD[i][1]&primeatt) == binaryFD[i][1])
-           {
-             continue;
-           }
-           else
-           {
-             boolean flag2NF=true;
-             for(int j=0;j<candidate.length;++j)
-             {
-                if((binaryFD[i][0]&candidate[j])== binaryFD[i][0] && (binaryFD[i][0]&candidate[j])!= candidate[j])
-                {
-                    flag2NF=false;
-                }
-             }
-             if (!flag2NF)
-             {
-              binaryFD[i][1]=( binaryFD[i][1]| primeatt)^primeatt;
-              boolean change=true;
-              while(change)
+            if((binaryFD[i][1]&primeatt) == binaryFD[i][1])
+            {
+              continue;
+            }
+            else
+            {
+              boolean flag2NF=true;
+              for(int j=0;j<candidate.length;++j)
               {
-                for(int j=0;j<binaryFD.length;++j)
-                {
-                    change=false;
-                    if((((binaryFD[j][0] | primeatt) ^ primeatt) & binaryFD[i][1] )!= 0 && (binaryFD[j][1] & binaryFD[i][1])!= binaryFD[j][1])//first remove prime attribues from left side
-                    {
-                        binaryFD[i][1]=binaryFD[i][1]|binaryFD[j][1];
-                        removed.add(j);
-                        change = true;
-                    }
-                }
-             }
-              not2NF.add(i);
-             }
-           }
-        }
+                 if((binaryFD[i][0]&candidate[j])== binaryFD[i][0] && (binaryFD[i][0]&candidate[j])!= candidate[j])
+                 {
+                     flag2NF=false;
+                 }
+              }
+              if (!flag2NF)
+              {
+               binaryFD[i][1]=( binaryFD[i][1]| primeatt)^primeatt;
+               boolean change=true;
+               while(change)
+               {
+                 for(int j=0;j<binaryFD.length;++j)
+                 {
+                     change=false;
+                     if((((binaryFD[j][0] | primeatt) ^ primeatt) & binaryFD[i][1] )!= 0 && (binaryFD[j][1] & binaryFD[i][1])!= binaryFD[j][1] && removed.indexOf(j)==-1)//first remove prime attribues from left side
+                     {
+                         int temp=(binaryFD[j][1] | primeatt)^primeatt;
+                         binaryFD[i][1]=binaryFD[i][1]|temp;
+                         removed.add(j);
+                         change = true;
+                     }
+                 }
+              }
+               not2NF.add(i);
+              }
+            }
+         }
         if(not2NF.size()==0 )
        {
            System.out.println("Relation is in 2NF");
        }
        else
        {
+           for(int i=0;i<not2NF.size();i++)
+            System.out.println(not2NF.get(i));
            System.out.println("false");
             convert_2NF(not2NF,attribute,binaryFD,removed);
        }
@@ -106,7 +110,6 @@ public class twoNF
                 {
                  removedflag=true;
                 }
-
             }
             if(!removedflag)
             {
@@ -114,6 +117,14 @@ public class twoNF
             }
             ArrayList<String>temp =new ArrayList<String>();
             relation.add(temp);
+        }
+        for(int i=0;i<binaryFD.length;i++)
+        {
+            if(removed.indexOf(i)==-1 && not2NF.indexOf(i)==-1)
+            {
+                k=k|binaryFD[i][0];
+                k=k|binaryFD[i][1];
+            }
         }
         for(int i=0;i<attribute.length;++i)
         {
